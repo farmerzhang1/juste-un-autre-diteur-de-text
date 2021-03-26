e8ddb2c52ac7a87089fd68c629866059e2390948
farmerzhang1 <zhang2000dc@gmail.com>
2021-03-26 19:35:20 +0800

    installation, hello world

---
+++ learning/test2.cpp
@@ -0,0 +1,17 @@
+#include <nana/gui.hpp>
+int main()
+{
+    using namespace nana;
+    form fm;
+    drawing{ fm }.draw([](paint::graphics& graph)
+    {
+        std::string hw = "Hello, world!";
+        auto hw_size = graph.text_extent_size(hw);
+        graph.string(
+            point{ static_cast<int>(graph.width() - hw_size.width) / 2,
+                    static_cast<int>(graph.height() - hw_size.height) / 2}
+            , hw);
+    });
+    fm.show();
+    exec();
+}
